479c9b504f4aacabc82deeff439ba4695a0ec940
Jack Babb <zendergo2@gmail.com>
2018-01-27 17:47:59 -0600

    Use gapps to make development easier

--- src/Code.js
+++ src/Code.js
@@ -42,7 +42,7 @@ function showCSV() {
 function saveSettings (type, settings) {
   var documentProperties = PropertiesService.getDocumentProperties(),
       f_settings = {};
-  
+
   f_settings[type+'_settings'] = JSON.stringify(settings);
   documentProperties.setProperties(f_settings);
   return true;
@@ -86,22 +86,22 @@ function _getCSV(input) {
   var data = [],
       heads = [],
       i = 0;
-  
+
   // get data from each form
   input.forEach(function(elem, idx) {
     data.push(getData(elem));
   });
   // data in form of:
   //   [form #][col #][row|row_link][row #] = cell value
-  
+
   var forms = [];
-  
+
   // for each form f = [form #]
   for (var f = 0; f < data.length; f++) {
     var csv = '';
-    
+
     /** For column titles **/
-    
+
     // for each column c = [col #]
     for (var c = 0; c < data[f].length; c++) {
       var title = data[f][c]['title'];
@@ -109,20 +109,20 @@ function _getCSV(input) {
     }
     // remove trailing comma
     csv = csv.substring(0, csv.length - 1);
-    
+
     /** For data rows **/
-    
+
     // for each row r = [row #]
     // All rows should be the same number of items
     for (var r = 0; r < data[f][0]['row'].length; r++) {
       csv += '\n'
-      
+
       // for each column c = [col #]
       for (var c = 0; c < data[f].length; c++) {
-        
+
         var row = data[f][c]['row'][r],
             row_link = '';
-        
+
         if (data[f][c]['row_link']) {
           row_link = '' + data[f][c]['row_link'][r];
           csv += '"=HYPERLINK(\"\"' + row_link + '\"\", \"\"' + row + '\"\")",';
@@ -134,7 +134,7 @@ function _getCSV(input) {
       // remove trailing comma
       csv = csv.substring(0, csv.length - 1);
     }
-    
+
     forms.push(csv);
   }
   return forms;
@@ -144,43 +144,43 @@ function _getXML(input) {
   var data = [],
       heads = [],
       i = 0;
-  
+
   // get data from each form
   input.forEach(function(elem, idx) {
     data.push(getData(elem));
   });
   // data in form of:
   //   [form #][col #][row|row_link][row #] = cell value
-  
+
   var forms = [];
-  
+
   // for each form f = [form #]
   for (var f = 0; f < data.length; f++) {
     var xml = '<Sheet' + (f + 1) + '>';
-    
+
     // for each row r = [row #]
     // All rows should be the same number of items
     for (var r = 0; r < data[f][0]['row'].length; r++) {
       xml += '<_' + (r + 1);
-      
+
       // for each column c = [col #]
       for (var c = 0; c < data[f].length; c++) {
-        
+
         var title = data[f][c]['title'].replace(/ /g, '_'),
             row = data[f][c]['row'][r],
             row_link = '';
-        
+
         xml += ' ' + title + '="' + row + '"';
-        
+
         if (data[f][c]['row_link']) {
           row_link = data[f][c]['row_link'][r];
           xml += ' ' + title + '_link="' + row + '"';
         }
       }
-      
+
       xml += '/>';
     }
-    
+
     xml += '</Sheet' + (f + 1) + '>';
     forms.push(xml);
   }
@@ -191,41 +191,41 @@ function _getHTML(input) {
   var data = [],
       heads = [],
       i = 0;
-  
+
   // get data from each form
   input.forEach(function(elem, idx) {
     data.push(getData(elem));
   });
   // data in form of:
   //   [form #][col #][row|row_link][row #] = cell value
-  
+
   var forms = [];
-  
+
   // for each form f = [form #]
   for (var f = 0; f < data.length; f++) {
     var html_head = '<thead><tr>',
         html_body = '<tbody>';
-    
+
     /** For column titles **/
-    
+
     // for each column c = [col #]
     for (var c = 0; c < data[f].length; c++) {
       var title = data[f][c]['title'];
       html_head += '<th>'+title+'</th>';
     }
-    
+
     /** For data rows **/
-    
+
     // for each row r = [row #]
     for (var r = 0; r < data[f][0]['row'].length; r++) {
       html_body += '<tr>';
-      
+
       // for each column c = [col #]
       for (var c = 0; c < data[f].length; c++) {
-        
+
         var row = data[f][c]['row'][r],
             row_link = '';
-        
+
         if (data[f][c]['row_link']) {
           row_link = '' + data[f][c]['row_link'][r];
           if (row_link.search('http') === -1) {
@@ -237,14 +237,14 @@ function _getHTML(input) {
           html_body += '<td>' + row + '</td>';
         }
       }
-      
+
       html_body += '</tr>';
     }
-    
-    
+
+
     html_head += '</tr></thead>';
-    html_body += '</tbody>';    
-    
+    html_body += '</tbody>';
+
     forms.push('<table>' + html_head + html_body + '</table>');
   }
   return forms;
@@ -254,46 +254,46 @@ function _getJSON(input) {
   var data = [],
       heads = [],
       i = 0;
-  
+
   // get data from each form
   input.forEach(function(elem, idx) {
     data.push(getData(elem));
   });
   // data in form of:
   //   [form #][col #][row|row_link][row #] = cell value
-  
+
   var forms = [];
-  
+
   // for each form f = [form #]
   for (var f = 0; f < data.length; f++) {
     var rows = [];
-    
+
     // for each row r = [row #]
     // All rows should be the same number of items
     for (var r = 0; r < data[f][0]['row'].length; r++) {
       var cells = {};
-      
+
       // for each column c = [col #]
       for (var c = 0; c < data[f].length; c++) {
-        
+
         // Add cell to object (title of col -> value of each row)
         var title = data[f][c]['title'].toLowerCase(),
             row = data[f][c]['row'][r],
             row_link = '';
-        
+
         cells[title] = row[0];
-        
+
         if (data[f][c]['row_link']) {
           row_link = data[f][c]['row_link'][r];
           cells[title + '_link'] = row_link[0];
         }
       }
-      
+
       if (cells) {
         rows.push(cells);
       }
     }
-    
+
     if (rows) {
       forms.push(rows);
     }
@@ -331,10 +331,10 @@ function getData(form) {
       default:
         // unknown
         break;
-    } 
+    }
   });
   // data now contains all form data
-  
+
   //  regex for matching and capturing "{A, B}, C..." etc.
   var regex = /(?:(\w+)|{(\w+)[, ]*(\w+)})/g,
       matches,
@@ -353,8 +353,8 @@ function getData(form) {
       columns[i] = {'text_col': matches[1].toLowerCase().charCodeAt(0) - 96};
   }
   // columns now contains json for columns to use
-  
-      // (optional) Sheet "sheet-name" or current sheet if no name given 
+
+      // (optional) Sheet "sheet-name" or current sheet if no name given
   var sheet = (data['sheet-id'] ? getSheetById(data['sheet-id']) : SpreadsheetApp.getActiveSheet()),
       // (optional) column titles from form or sheet (or nothing if both don't exist);
       titles = (data['titles'] ? data['titles'].split(/, */) : getTitles(sheet, columns)),
@@ -368,11 +368,11 @@ function getData(form) {
       filterColData = (filterCol ? sheet.getRange(dataRowStart, filterCol, size).getValues() : false),
       // (optional) strings for filtering or false
       filterCriteria = (filterCol && data['filter-criteria'] ? data['filter-criteria'].split(/, */): false);
-  
+
   if (titles === false) {
     throw 'Required information not provided';
   }
-  
+
   if (data['remove-empty'] === true) {
     if (filterCriteria === false) {
       filterCol = 1;
@@ -381,7 +381,7 @@ function getData(form) {
     }
     filterCriteria.push("");
   }
-  
+
   i = 0;
   while (columns[i]) {
     columns[i].title = titles[i];
@@ -398,7 +398,7 @@ function getData(form) {
   return columns;
 }
 
-// Takes a column, filter column, filter to removes filtered rows from currCol 
+// Takes a column, filter column, filter to removes filtered rows from currCol
 function colFilter(currCol, filterCol, filter) {
   if (filterCol === false || filter === false) {
     return currCol;
@@ -418,11 +418,11 @@ function colFilter(currCol, filterCol, filter) {
 function getTitles (sheet, columns) {
   var allCols = sheet.getRange('A1:1'),
       result = [];
-  
+
   for (var i = 0; i < columns.length; i++) {
     if (columns[i].text_col) {
       var val = allCols.getCell(1, columns[i].text_col).getValue();
-      
+
       // If we need to look at cells for titles and at least one is blank, throw error
       if (val === "") {
         return false;
@@ -430,7 +430,7 @@ function getTitles (sheet, columns) {
       result.push(val);
     }
   }
-  
+
   if (result.length === 0) {
     return false;
   }
@@ -464,7 +464,7 @@ function countCol (sheet, col, rowStart) {
   numRows = range.getNumRows();
   off = 1;
   var tempCount = 0;
-  
+
   while (tempCount < numRows) {
     if (range.isBlank()) {
       break;
@@ -480,7 +480,7 @@ function countCol (sheet, col, rowStart) {
 
 function getSheetById(id) {
   var sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
-  
+
   for (var i = 0; i < sheets.length; i++) {
     if (sheets[i].getSheetId() === parseInt(id)) {
       return sheets[i];
